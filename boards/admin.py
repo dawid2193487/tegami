f4e65a77d0f14fb3ae5ebf4d3fb0ca945759e20d
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-01 20:33:31 +0100

    Add Board and Thread models

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    pass
--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=20)
+    description = models.CharField(verbose_name="Description", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Thread(models.Model):
+    message = models.TextField(verbose_name="Message", max_length=6000)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message[:100]
