909ecb10c161f1530df35f44f47965b78c196692
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-20 17:54:12 +0100

    Add a Board serializer

--- backend/boards/serializers.py
+++ backend/boards/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import Reply, Thread, Board
 
-class ReplySerializer(serializers.ModelSerializer):
+class BoardSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Reply
-        fields = ['pk', 'posted_by', 'message', 'reply_in', 'posted_at']
+        model = Board
+        fields = ['name', 'description', 'slug', 'created_at']
 
 class ThreadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Thread
         fields = ['pk', 'posted_by', 'message', 'board', 'posted_at']
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['pk', 'posted_by', 'message', 'reply_in', 'posted_at']
