c39d618b1dfbb3f09aebba7f81f105329398fd56
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-20 17:16:10 +0100

    Add a profile model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Profile(models.Model):
+    display_name = models.CharField(max_length=48)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, models.CASCADE)
