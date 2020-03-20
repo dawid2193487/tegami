25fa4b4e59a6b8a2c2ccb4c0b0e7decf252213eb
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-20 17:15:52 +0100

    Add a profile detail call

--- backend/boards/consumers.py
+++ backend/boards/consumers.py
@@ -1,10 +1,43 @@
+from django.forms.models import model_to_dict
+from django.contrib.auth import get_user_model
 from channels.generic.websocket import JsonWebsocketConsumer
 from channels.exceptions import StopConsumer
-from .models import Thread
-from django.forms.models import model_to_dict
 from asgiref.sync import async_to_sync
 
-class ThreadConsumer(JsonWebsocketConsumer):
+from .models import Thread
+
+class InvalidParameters(Exception):
+    pass
+
+class AccessConsumer(JsonWebsocketConsumer):
+    def receive_json(self, content):
+        t = content["type"]
+        # without the "tegami_" prefix below, this could be exploited as an RCE
+        func = getattr(self, f'tegami_{t}', None)
+        if func is None:
+            self.send_json({
+                "type": "no_such_call",
+                "call_copy": content
+            })
+            return
+
+        try:
+            func(content)
+        except (InvalidParameters, KeyError):
+            self.send_json({
+                "type": "invalid_call",
+                "call_copy": content
+            })
+
+    def tegami_get_profile(self, content):
+        user = get_user_model().objects.get(pk=content["pk"])
+        profile = user.profile
+        self.send_json({
+            "type": "profile_detail",
+            "profile": model_to_dict(profile),
+        })
+
+class ThreadConsumer(AccessConsumer):
 
     def connect(self):
         self.accept()
@@ -18,9 +51,6 @@ class ThreadConsumer(JsonWebsocketConsumer):
             "thread": model_to_dict(thread),
             "replies": replies
         })
-        
-    def receive_json(self, content):
-        pass
 
     def new_reply(self, event):
         self.send_json({
@@ -30,4 +60,7 @@ class ThreadConsumer(JsonWebsocketConsumer):
 
     def disconnect(self, code):
         async_to_sync(self.channel_layer.group_discard)(self.chan_id, self.channel_name)
+        self.send_json({
+            "type": "disconnect",
+        })
         raise StopConsumer
