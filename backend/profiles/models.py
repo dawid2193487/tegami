198a8bb69571d9a06341da3360d681d671c0a844
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-20 18:23:43 +0100

    Make profiles auto-create

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
     display_name = models.CharField(max_length=48)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, models.CASCADE)
+
+    def __str__(self):
+        return f"{self.display_name} ({self.user.username})"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
