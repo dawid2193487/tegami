c436bcc393744989dfae301718bba09e5c6fcbb4
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-22 22:03:44 +0100

    Add Nonce model

---
+++ backend/nonce/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NonceConfig(AppConfig):
+    name = 'nonce'
--- backend/tegami/settings.py
+++ backend/tegami/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'boards',
     'attach',
     'profiles',
+    'nonce',
 ]
 
 MIDDLEWARE = [
