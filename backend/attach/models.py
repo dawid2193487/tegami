269d9c0b7758847b43af6b7c7fe6e5b36049f2f9
Dawid Pietrzak <dawidpietrzak000+github@gmail.com>
2020-03-24 17:29:16 +0100

    Add a thumbnail field

--- backend/attach/models.py
+++ backend/attach/models.py
@@ -5,6 +5,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class Attachment(models.Model):
     upload = models.FileField()
+    thumbnail = models.FileField(null=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
